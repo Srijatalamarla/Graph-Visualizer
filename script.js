adbfc3d99714cf695c383f891bb431e18e3850d8
Srija <talamarlasrija@gmail.com>
2025-10-21 01:26:05 +0530

    added feedback form and some things in main

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const footerForm = document.querySelector('.footer-form-container');
+
+function openFooterForm() {
+    footerForm.style.display = 'flex';
+}
+
+function closeFooterForm() {
+    footerForm.style.display = 'none';
+}
+
+document.addEventListener('click', (e) => {
+    if (e.target == footerForm)
+        closeFooterForm();
+})
