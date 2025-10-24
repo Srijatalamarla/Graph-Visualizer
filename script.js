4afeafc6f30603553f5a39737f16f873657477d2
Srija <talamarlasrija@gmail.com>
2025-10-25 00:24:27 +0530

    Added input fields for graph input

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 
+const graphInputArea = document.getElementById('graph-input-area');
+const graphWeightRadios = document.querySelectorAll('input[name="graph-weight"]');
+
+function updateGraphPlaceholder() {
+    const selectedWeight = document.getElementById('weighted').checked;
+    console.log("update")
+
+    if (selectedWeight) {
+        // Weighted graph placeholder: node1 node2 weight
+        graphInputArea.placeholder = `Example:\nNode Node Weight\n1 2 5\n3 2 7\n2 3 4\n4 5 6`;
+    } else {
+        // Non-weighted graph placeholder: node1 node2
+        graphInputArea.placeholder = `Example:\nNode Node\n1 2\n3 2\n2 3\n4 5`;
+    }
+    
+}
+
+// Set placeholder on page load
+document.addEventListener('DOMContentLoaded', updateGraphPlaceholder);
+
+// Update placeholder on radio change
+graphWeightRadios.forEach(radio => radio.addEventListener('change', updateGraphPlaceholder));
+
+
+/*------- footer form related functionality ------*/
+
 const footerForm = document.querySelector('.footer-form-container');
 
 function openFooterForm() {
@@ -12,4 +38,6 @@ function closeFooterForm() {
 document.addEventListener('click', (e) => {
     if (e.target == footerForm)
         closeFooterForm();
-})
+});
+
+/*------- footer form related functionality ------*/
